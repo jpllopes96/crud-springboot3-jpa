23071ffdf0c3929ca71afc5f59f2a014d4e9cea8
jpllopes96 <joaolacaia96@gmail.com>
2023-04-13 18:48:48 -0300

    Subtotal and Total methods

--- src/main/java/com/educationweb/course/entities/OrderItem.java
+++ src/main/java/com/educationweb/course/entities/OrderItem.java
@@ -63,6 +63,9 @@ public class OrderItem implements Serializable{
         id.setProduct(product);
     }
 
+    public Double getSubtotal(){
+        return price * quantity;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -75,4 +78,6 @@ public class OrderItem implements Serializable{
     public int hashCode() {
         return Objects.hash(id);
     }
+
+
 }
